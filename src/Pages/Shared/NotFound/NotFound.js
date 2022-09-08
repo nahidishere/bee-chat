05c3171e3a74d9a0cb14bb82a311df058f17c610
Nahid Hasan <dev.nahid.hasan@gmail.com>
2022-09-08 09:01:28 -0400

    Not found page added

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,20 @@
 import React from "react";
+import notFound from "../../../assets/notfound.jpg";
+import cantFind from "../../../assets/404.png";
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Sorry page not found </h2>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          src={cantFind}
+          className="max-w-sm rounded-lg shadow-2xl"
+          alt="Cant find"
+        />
+        <div>
+          <img src={notFound} alt="Not found page" />
+        </div>
+      </div>
     </div>
   );
 };
